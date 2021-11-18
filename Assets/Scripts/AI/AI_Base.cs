1fffd73278dd570071713b963ead689fdeabf46c
Charles Serrato <serrato.charles@gmail.com>
2021-11-18 18:40:55 +0800

    AI Flight slightly refined + Added distance before maneuvering + Added more flag checks before adjusting flight path + Changed values to make roll and yaw more accurate To-do - Check why on certain angles, ai does a spiral movement before proceeding forward to the target - Might have something to do with pitch adjustment

--- Assets/Scripts/AI/AI_Base.cs
+++ Assets/Scripts/AI/AI_Base.cs
@@ -6,20 +6,28 @@ using TMPro;
 public class AI_Base : MonoBehaviour
 {
     // Parameters
+    [Header("Ship Properties")]
     [SerializeField] float minSpeed = 2f;
     [SerializeField] float maxSpeed = 5f;
-    [SerializeField] float pitchSpeed = 5f;
-    [SerializeField] float rollSpeed = 5f;
-    [SerializeField] float yawSpeed = 5f;
-    [SerializeField] float turnSmoothTime = 5f;
+    [Header("Ship Controls")]
+    [SerializeField] float yawSpeed = .45f;
+    [SerializeField] float pitchSpeed = .8f;
+    [SerializeField] float rollSpeed = 2f;
+    [Header("AI Behavior")]
+    [SerializeField] float maxDistanceBeforeTurning = 150f;
+    [SerializeField] float randomizeFactor = 0.35f;
 
     // State
-    private float turnSmoothVelocity;
+    [Header("Ship Status")]
     [SerializeField] private float currentSpeed = 5f;
     [SerializeField] private GameObject currentTarget;
     private bool targetIsBehind;
+    private float distanceToEnableTurning;
+    private bool hasRandomized = false;
 
     // DEBUG
+    [Header("DEBUG")]
+    [SerializeField] bool debugMode = false;
     [SerializeField] TextMeshProUGUI rightDotText;
     [SerializeField] TextMeshProUGUI upDotText;
     [SerializeField] TextMeshProUGUI upToDirDotText;
@@ -32,10 +40,16 @@ public class AI_Base : MonoBehaviour
     [SerializeField] float rollAngleDebug;
     [SerializeField] float pitchAngleDebug;
     [SerializeField] float yawAngleDebug;
+    [SerializeField] float distanceFromTarget;
 
     private void Awake()
     {
-        
+        distanceToEnableTurning = maxDistanceBeforeTurning;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(RandomizeDistance());
     }
 
     private void Update()
@@ -46,8 +60,8 @@ public class AI_Base : MonoBehaviour
     private void Move()
     {
         /* THRUST */
-        transform.Translate(Vector3.forward * currentSpeed * Time.deltaTime);
         // TODO - make AI adjust thrust when banking. Do this after finishing roll and pitch code
+        transform.Translate(Vector3.forward * currentSpeed * Time.deltaTime);
 
         // Target check
         if (currentTarget == null) { return; }
@@ -55,6 +69,7 @@ public class AI_Base : MonoBehaviour
         // Compute for Distance and Direction to target
         Vector3 distToTarget = currentTarget.transform.position - transform.position;
         Vector3 dirToTarget = distToTarget.normalized;
+        distanceFromTarget = distToTarget.magnitude;
 
         // Get 'center' angle between agent and target
         Vector3 cross = Vector3.Cross(transform.position, currentTarget.transform.position);
@@ -66,7 +81,7 @@ public class AI_Base : MonoBehaviour
         float forwardDot = Vector3.Dot(transform.forward, dirToTarget);
 
         // Check if target is behind the agent.
-        targetIsBehind = forwardDot < 0f ? true : false;
+        targetIsBehind = forwardDot < 0.5f ? true : false;
 
         /* ***ROLL*** */
         // Get amount of angle to rotate
@@ -76,7 +91,7 @@ public class AI_Base : MonoBehaviour
         if (roll)
         {
             // Stop rotating when near target angle to prevent stutter
-            if (Mathf.Abs(upDot) > 0.05f)
+            if (Mathf.Abs(upDot) > 0.05f) // Number closer to 0 makes adjustment more accurate
             {
                 transform.Rotate(Vector3.back * rollAngle * rollSpeed * Time.deltaTime);
             }
@@ -88,11 +103,16 @@ public class AI_Base : MonoBehaviour
         pitchAngleDebug = pitchAmount; // Debug only
         if (pitch)
         {
-            // Stop rotating when near target angle to prevent stutter
-            // Only stop rotating if target is in front of the object (ensures target is moving TO target)
-            if (Mathf.Abs(upToDirDot) > 0.05f || targetIsBehind)
+            // TODO: Check how to make this rotate downwards. It seems like ai only pitches up
+            // Rotate if target is behind or if target pitch angle is not yet near target angle
+            if (Mathf.Abs(upToDirDot) > 0.035f || targetIsBehind) // Number closer to 0 makes adjustment more accurate
             {
-                transform.Rotate(Vector3.left * Mathf.Abs(pitchAmount) * pitchSpeed * Time.deltaTime);
+                // Rotate if max distance reached. Makes flight look more convincing
+                // Also rotate while target is in front and target pitch angle is not yet reached
+                if (distanceFromTarget > distanceToEnableTurning || !targetIsBehind)
+                {
+                    transform.Rotate(Vector3.left * Mathf.Abs(pitchAmount) * pitchSpeed * Time.deltaTime);
+                }
             }
         }
 
@@ -102,21 +122,36 @@ public class AI_Base : MonoBehaviour
         yawAngleDebug = yawAmount; // Debug only
         if (yaw)
         {
-            // Stop rotating when near target angle to prevent stutter
-            // Only stop rotating if target is in front of the object (ensures target is moving TO target)
-            if (Mathf.Abs(rightToDirDot) > 0.05f || targetIsBehind)
+            // Rotate if target is behind or if target yaw angle is not yet near target angle
+            if (Mathf.Abs(rightToDirDot) > 0.035f || targetIsBehind) // Number closer to 0 makes adjustment more accurate
             {
-                transform.Rotate(Vector3.down * Mathf.Abs(yawAmount) * yawSpeed * Time.deltaTime);
+                // Rotate if max distance reached. Makes flight look more convincing
+                // Also rotate while target is in front and target yaw angle is not yet reached
+                if (distanceFromTarget > distanceToEnableTurning || !targetIsBehind)
+                {
+                    transform.Rotate(Vector3.down * Mathf.Abs(yawAmount) * yawSpeed * Time.deltaTime);
+                }
             }
         }
 
+        /* ***UNSTUCK*** */
+        // Instant look to target to prevent death spiral bug
+        if (distanceFromTarget > 500f && targetIsBehind)
+        {
+            Debug.Log(gameObject.name + " prevented death spiral!");
+            transform.LookAt(currentTarget.transform, transform.up);
+        }
+
         /** !DEBUG UI TEXT! **/
-        rightDotText.SetText("RDot: " + rightDot.ToString());
-        upDotText.SetText("UDot: " + upDot.ToString());
-        upToDirDotText.SetText("UDirDot: " + upToDirDot.ToString());
-        rightToDirDotText.SetText("RDirDot: " + rightToDirDot.ToString());
-        targetBehind.SetText("Target Behind?: " + targetIsBehind.ToString());
-        targetAngle.SetText("Angle: " + forwardDot.ToString());
+        if (debugMode)
+        {
+            rightDotText.SetText("RDot: " + rightDot.ToString());
+            upDotText.SetText("UDot: " + upDot.ToString());
+            upToDirDotText.SetText("UDirDot: " + upToDirDot.ToString());
+            rightToDirDotText.SetText("RDirDot: " + rightToDirDot.ToString());
+            targetBehind.SetText("Target Behind?: " + targetIsBehind.ToString());
+            targetAngle.SetText("Angle: " + forwardDot.ToString());
+        }
 
         /** !DEBUG LINES! **/
         // Local Up
@@ -127,8 +162,27 @@ public class AI_Base : MonoBehaviour
         Debug.DrawLine(transform.position, transform.position + transform.forward * 15f, Color.blue);
         // Dir to target
         Debug.DrawLine(transform.position, transform.position + (dirToTarget * 15f), Color.yellow);
+        //Debug.DrawLine(transform.position, currentTarget.transform.position, Color.yellow);
         // Cross (of this object to target)
         Debug.DrawLine(transform.position, transform.position + (cross.normalized * 15f), Color.cyan);
+    }
+
+    private IEnumerator RandomizeDistance()
+    {
+        while (true)
+        {
+            if (!hasRandomized && !targetIsBehind)
+            {
+                float randomFactor = maxDistanceBeforeTurning - (maxDistanceBeforeTurning * randomizeFactor);
+                distanceToEnableTurning = Random.Range(randomFactor, maxDistanceBeforeTurning);
+                hasRandomized = true;
+            }
+            if (targetIsBehind)
+            {
+                hasRandomized = false;
+            }
+            yield return null;
+        }
     }
 
     //TODO: Create the following Base AI Methods
